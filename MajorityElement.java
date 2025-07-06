dc3ec3a20810d24d363bf76192d8bbad222554b3
Yogeshwari-VV <vvyogam2004@gmail.com>
2025-07-06 13:52:09 +0530

    169-Majority Element

---
+++ MajorityElement.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map= new HashMap<>();
+        int n=nums.length;
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+            if(entry.getValue()>n/2){
+                return entry.getKey();
+            }
+        }
+        return -1;
+        
+    }
+}
