22ca72dad8f2b5b1a9e72aef64784194fdeaea67
Yogeshwari-VV <vvyogam2004@gmail.com>
2025-12-31 19:58:45 +0530

    1346-Check If N and Its Double Exist

---
+++ NandDouble.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for(int x: arr){
+            if(set.contains(2*x) || (x%2==0 && set.contains(x/2))){
+                return true; 
+        }
+        set.add(x);
+        }
+        return false;
+
+        
+    }
+}
