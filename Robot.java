72aa72887d5f6418808102211047c0ade3e439b2
Yogeshwari-VV <vvyogam2004@gmail.com>
2026-04-05 19:26:14 +0530

    657. Robot Return to Origin

---
+++ Robot.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        for(char move : moves.toCharArray()){
+            if(move == 'U') x++;
+            else if(move == 'D') x--;
+            else if(move == 'R') y++;
+            else if(move == 'L') y--;
+
+        }
+        return x==0 && y==0; 
+        
+    }
+}
