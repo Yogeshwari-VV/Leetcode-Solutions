4d6180ab71b391cb9bf3e0d9059bcc7832dbf422
Yogeshwari-VV <vvyogam2004@gmail.com>
2026-06-22 11:14:12 +0530

    2390. Removing Stars From a String

---
+++ RemStars.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='*'){
+                st.pop();
+            }else{
+                st.push(ch);
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+        while(!st.isEmpty()){
+            ans.append(st.pop());
+        }
+        return ans.reverse().toString();
+    }
+}
